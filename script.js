63643b5d8c697e6ea191b85bd4bfdda0d973f994
Denis Duarte <duarte.denis11@gmail.com>
2020-05-18 22:01:51 -0400

    Everything is done except for skills

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-// TODO: add code here
+function init() {
+
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        response.json().then(function(json) {
+            console.log(json);
+            let div = document.getElementById("container");
+            for (let i = 0; i < json.length; i++) {
+                div.innerHTML += `
+                    <div class="astronaut">
+                    <div class="bio">
+                    <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                    <ul>
+                        <li>Hours in space: ${json[i].hoursInSpace}</li>
+                        <li>Active: ${json[i].active}</li>
+                        <li>Skills: Physician, Chemical Engineer</li>
+                    </ul>
+                    </div>
+                    <img class="avatar" src=${json[i].picture}>
+                </div>
+                `;
+            }
+
+        });
+
+
+    });
+
+};
+
+
+
+
+
+window.onload = init;
